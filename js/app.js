118c7ccd4ec9a8be956fc310a2f898a113f09ac4
Robertoam <ram3015@hotmail.com>
2019-02-03 23:26:22 -0600

    Proyecto sin analitys

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+$(document).ready(function(){
+   $('.parallax').parallax();
+});
+
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true
+});
